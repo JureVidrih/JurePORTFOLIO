9ba066f0ab301597c5eccb20ffaf0cec539a72b1
JureVidrih <jurevidrih@gmail.com>
2025-08-13 00:40:52 +0200

    Add waypoint and transition effects for skills list

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 const button_scroll_to_top = document.querySelector("div.scroll-to-top-button");
 const h1 = document.querySelector("h1");
 const top_navbar = document.querySelector("body>header>nav");
-const section_skills = document.querySelector("section#skills");
 
 const h1_waypoint = new Waypoint({
     element: h1,
@@ -15,8 +14,21 @@ const h1_waypoint = new Waypoint({
 const top_navbar_waypoint = new Waypoint({
     element: top_navbar,
     handler: function(direction) {
-        if(direction== "up" && button_scroll_to_top.classList.contains("scroll_to_top_button_visible")) {
+        if(direction == "up" && button_scroll_to_top.classList.contains("scroll_to_top_button_visible")) {
             button_scroll_to_top.classList.toggle("scroll_to_top_button_visible");
         }
     }
+});
+
+const section_skills = document.querySelector("section#skills");
+const section_skills_list = document.querySelector("section#skills-list");
+
+const section_skills_list_waypoint = new Waypoint({
+    element: section_skills_list,
+    handler: function(direction) {
+        if(direction == "down" && !section_skills_list.classList.contains("visible")) {
+            section_skills_list.classList.toggle("visible");
+        }
+    },
+    offset: "bottom-in-view"
 });
