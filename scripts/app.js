e950dc4832b8cc165c7e9b60372a7b8ad474232c
JureVidrih <jurevidrih@gmail.com>
2025-08-18 23:53:20 +0200

    Work on behavior and styling of projects

--- scripts/app.js
+++ scripts/app.js
@@ -32,3 +32,47 @@ const section_skills_list_waypoint = new Waypoint({
     },
     offset: "bottom-in-view"
 });
+
+const button_busyn = document.querySelector("#button-busyn");
+const project_busyn = document.querySelector("#project-busyn");
+const project_busyn_height = project_busyn.scrollHeight;
+
+project_busyn.style.transition = "max-height 1.5s ease-out";
+
+button_busyn.addEventListener('click', () => {
+    button_busyn.classList.toggle("button-project-more-active");
+    project_busyn.style["max-height"] = (project_busyn.style["max-height"] == project_busyn_height+"px" ? null : project_busyn_height+"px");
+});
+
+const button_repl = document.querySelector("#button-repl");
+const project_repl = document.querySelector("#project-repl");
+const project_repl_height = project_repl.scrollHeight;
+
+project_repl.style.transition = "max-height "+(1.5*(project_repl_height/project_busyn_height))+"s ease-out";
+
+button_repl.addEventListener('click', () => {
+    button_repl.classList.toggle("button-project-more-active");
+    project_repl.style["max-height"] = (project_repl.style["max-height"] == project_repl_height+"px" ? null : project_repl_height+"px");
+});
+
+const button_eval = document.querySelector("#button-eval");
+const project_eval = document.querySelector("#project-eval");
+const project_eval_height = project_eval.scrollHeight;
+
+project_eval.style.transition = "max-height "+(1.5*(project_eval_height/project_busyn_height))+"s ease-out";
+
+button_eval.addEventListener('click', () => {
+    button_eval.classList.toggle("button-project-more-active");
+    project_eval.style["max-height"] = (project_eval.style["max-height"] == project_eval_height+"px" ? null : project_eval_height+"px");
+});
+
+const button_logger = document.querySelector("#button-logger");
+const project_logger = document.querySelector("#project-logger");
+const project_logger_height = project_logger.scrollHeight;
+
+project_logger.style.transition = "max-height "+(1.5*(project_logger_height/project_busyn_height))+"s ease-out";
+
+button_logger.addEventListener('click', () => {
+    button_logger.classList.toggle("button-project-more-active");
+    project_logger.style["max-height"] = (project_logger.style["max-height"] == project_logger_height+"px" ? null : project_logger_height+"px");
+});
